d6527d2e570ea3a9127d9c27268f5bea6337384f
kuroda-kentaro <kuroda_business@outlook.jp>
2019-04-02 16:58:11 +0900

    create_actions

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios'
+export const READ_TODOS = 'READ_TODOS'
+
+const ROOT_URL = 'https://3c0sh3pmt4.execute-api.ap-northeast-1.amazonaws.com/dev/crud'
+
+export const readTodos = () => async dispatch => {
+    const response =  await axios.get(`${ROOT_URL}`)
+    console.log(response);
+    dispatch({ type: READ_TODOS, response })
+}
