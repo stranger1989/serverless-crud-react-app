9115a87056da3ccbac690c6a41df367aa42f22a2
kuroda-kentaro <kuroda_business@outlook.jp>
2019-04-10 17:26:53 +0900

    write_devtools_and_revise_some_routing

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
-import {BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 import './index.css';
 import reducer from './reducers';
@@ -11,13 +12,17 @@ import reducer from './reducers';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === 'development' ?
+    composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <Switch>
                 <Route exact path="/" component={ App } />
+                <Route exact path="/todo/new" component={ App } />
+                <Route exact path="/todo/:id" component={ App } />
             </Switch>
         </BrowserRouter>
     </Provider>,
