a9af15fbef9cd693bbf38a1963a2dc28f684944b
kuroda-kentaro <kuroda_business@outlook.jp>
2019-04-10 17:39:11 +0900

    create_read_update_delete_action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 export const READ_TODOS = 'READ_TODOS'
+export const READ_TODO = 'READ_TODO'
 export const CREATE_TODO = 'CREATE_TODO'
+export const UPDATE_TODO = 'UPDATE_TODO'
+export const DELETE_TODO = 'DELETE_TODO'
 
 const ROOT_URL = 'https://3c0sh3pmt4.execute-api.ap-northeast-1.amazonaws.com/dev/crud'
 
@@ -10,9 +13,21 @@ export const readTodos = () => async dispatch => {
 }
 
 export const postTodo = values => async dispatch => {
-    console.log(values);
-    console.log(ROOT_URL);
     const response =  await axios.post(`${ROOT_URL}`, values)
-    console.log(response);
     dispatch({ type: CREATE_TODO, response })
+}
+
+export const putTodo = values => async dispatch => {
+    const response =  await axios.put(`${ROOT_URL}/${values.id}`, values)
+    dispatch({ type: UPDATE_TODO, response })
+}
+
+export const getTodo = id => async dispatch => {
+    const response =  await axios.get(`${ROOT_URL}/${id}`)
+    dispatch( {type: READ_TODO, response} )
+}
+
+export const deleteTodo = id => async dispatch => {
+    await axios.delete(`${ROOT_URL}/${id}`)
+    dispatch({ type: DELETE_TODO, id })
 }
